f56e02c592d203116c9e3cd95324449e5258a21c
sibendu <sibendu@users.noreply.github.com>
2019-03-26 21:31:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var request = require('request');
 var handleRequest = function(request, response) {
   
   console.log('Calling hello-node-rest service'); 
-  request('http://hello-node-rest:5000/users, function (error, response, body)        {
+  request('http://hello-node-rest:5000/users', function (error, response, body){
     console.log('error:', error);
     console.log('statusCode:', response && response.statusCode);
     console.log('body:', body);
